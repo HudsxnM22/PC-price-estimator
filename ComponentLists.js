f87c609dd1c6c8579cc8309007be44b5448aaaf3
HudsxnM22 <maioranahudson@gmail.com>
2024-06-16 13:43:21 -0400

    refactored some

--- ComponentLists.js
+++ ComponentLists.js
@@ -1,118 +1,120 @@
 //populates the cpu and gpu dropdown menues based on brand chosen via j-query and Select2
 
+intelcpus = [], amdcpus = [], nvidiagpus = [], amdgpus = [], intelgpus = [];
 //grabs cpu array list from back end database
-const FetchCPUs = async () => {
+const FetchComponents = async () => {
     try{    
-        const response = await fetch('http://71.176.233.55:3566/cpus');
-        const cpus = await response.json();
-        return cpus;
+        var response = await fetch('http://71.176.233.55:3566/intel-cpus'); //fetches intel cpus and stores them into intel cpu array as strings
+        intelcpus = await response.json();
+        var response = await fetch('http://71.176.233.55:3566/amd-cpus'); //fetches amd cpus
+        amdcpus = await response.json();
+        var response = await fetch('http://71.176.233.55:3566/nvidia-gpus'); //[nvidia]fetches gpus and stores them into their respective arrays as strings
+        nvidiagpus = await response.json();
+        var response = await fetch('http://71.176.233.55:3566/amd-gpus');//[amd]
+        amdgpus = await response.json();
+        var response = await fetch('http://71.176.233.55:3566/intel-gpus');//[intel]
+        intelgpus = await response.json();
     }
     catch(error){
-        console.log('there was a problem with fetching CPUS', error);
+        console.log('there was a problem with fetching Components', error);
     }
 }
 
-//grabs gpu array list from back end database
-const FetchGPUs = async () => {
-    try{    
-        const response = await fetch('http://71.176.233.55:3566/gpus');
-        const gpus = await response.json();
-        return gpus;
-    }
-    catch(error){
-        console.log('there was a problem with fetching GPUS', error);
-    }
-}
+//organizes cpus into select2 objects so they can be used in the searchable dropdown menu
+const select2Intelcpus = [], select2Amdcpus = [], select2Nvidiagpus = [], select2Amdgpus = [], select2Intelgpus = [];
 
-//organize cpus based on brand Intel, AMD, Nvidia
-const gencpus = [], intelcpus = [], amdcpus = [], gengpus = [], nvidiagpus = [], amdgpus = [], intelgpus = [];
 
-//categorizes CPUs
-const categorizeCPUs = (cpus) => {
-
-    cpus.forEach(element => {
-        gencpus.push({
-            id: gencpus.length + 1,  //must store the elements as JSON objects in this format in order to populate the select2 dropdown menu.
-            text: element
+//categorizes CPUs and GPUs into their select2 arrays as the select2 variants of themselves
+const categorizeComponents = async (intelcpus, amdcpus, nvidiagpus, amdgpus, intelgpus) => { //must store the elements as JSON objects in this format in order to populate the select2 dropdown menu.
+    try{
+        intelcpus.forEach(element => { //CPUS
+            select2Intelcpus.push({ 
+                id: select2Intelcpus.length + 1, //must follow this format
+                text: element
+            });
         });
-
-        // Intel CPUs
-        if (element.charAt(0) === 'i' || 
-            (element.charAt(0) === 'P' && element.charAt(1) === 'e') || 
-            element.charAt(0) === 'X' || 
-            element.charAt(0) === 'C') {
-            intelcpus.push({
-                "id": intelcpus.length + 1,
-                "text": element
+        amdcpus.forEach(element => { 
+            select2Amdcpus.push({ 
+                id: select2Amdcpus.length + 1,
+                text: element
             });
-        }
-        // AMD CPUs
-        if (element.charAt(0) === 'R' || 
-            (element.charAt(0) === 'P' && element.charAt(1) === 'h') || 
-            element.charAt(0) === 'A' || 
-            element.charAt(0) === 'F') {
-            amdcpus.push({
-                "id": amdcpus.length + 1,
-                "text": element
+        });
+        nvidiagpus.forEach(element => { //GPUS
+            select2Nvidiagpus.push({ 
+                id: select2Nvidiagpus.length + 1,
+                text: element
             });
-        }
-    });
+        });  
+        amdgpus.forEach(element => { 
+            select2Amdgpus.push({ 
+                id: select2Amdgpus.length + 1,
+                text: element
+            });
+        }); 
+        intelgpus.forEach(element => { 
+            select2Intelgpus.push({ 
+                id: select2Intelgpus.length + 1,
+                text: element
+            });
+        }); 
+    }
+    catch(error){
+        console.log('error transfering arrays to select2 arrays')
+    }
 };
 
-//categroizes GPUS
-const categorizeGPUs = (gpus) => {
-    gpus.forEach(element => {
-        gengpus.push(element);
-        // Nvidia GPUs
-        if ((element.charAt(0) === 'R' && element.charAt(1) === 'T') || 
-            (element.charAt(0) === 'G' && element.charAt(1) === 'T') || 
-            (element.charAt(0) === 'T' && element.charAt(1) === 'i') || 
-            (element.charAt(0) === 'Q' && element.charAt(1) === 'u')) {
-            nvidiagpus.push(element);
-        }
-        // AMD GPUs
-        if ((element.charAt(0) === 'R' && element.charAt(1) !== 'T') || 
-            element.charAt(0) === 'V' ||  
-            (element.charAt(0) === 'F' && element.charAt(1) === 'i')) {
-            amdgpus.push(element);
-        }
-        // Intel GPUs
-        if (element.charAt(0) === 'A' || 
-            (element.charAt(0) === 'I' && element.charAt(1) === 'r') ||
-            (element.charAt(0) === 'H' && element.charAt(1) === 'D') || 
-            (element.charAt(0) === 'U' && element.charAt(1) === 'H')) {
-            intelgpus.push(element);
-        }
-    });
-};
+const initialize = async () => {
+    await FetchComponents();
+    await categorizeComponents(intelcpus, amdcpus, nvidiagpus, amdgpus, intelgpus);
 
-FetchCPUs().then(cpus => {
-    categorizeCPUs(cpus);
-}).then({
+    //filter tests
+    //console.log(intelcpus);
+    //console.log(amdcpus);
+    //console.log(nvidiagpus);
+    //console.log(amdgpus);
+    //console.log(intelgpus);
 
-
-    
-});
-
-FetchGPUs().then(gpus => {
-    categorizeGPUs(gpus);
-});
-
-//filter tests
-//console.log(intelcpus);
-//console.log(amdcpus);
-//console.log(gencpus);
-//console.log(nvidiagpus);
-//console.log(amdgpus);
-//console.log(intelgpus);
+    populateDropDown();
+}
 
 //populate the dropdown menu for cpus via J-query
-$(document).ready(async function(){
-    const $cpuSelect = await $("#cpu-select"); //stores jquery id of dropdown menu for cpus
-    $cpuSelect.select2({
-        data: gencpus,
-        allowClear: true, //add this into an async / await function
-        dropdownCssClass: 'component-select',
-        selectionCssClass: 'component-select', //fix this -----------------------------><><<><><><><><><><<><><><><><<>>
-    }); //turns it into a select2 select type so as to make the list searchable
+const populateDropDown = (displayArray) => {
+    $(document).ready( function(){
+        const $cpuSelect = $("#cpu-select"); //stores jquery id of dropdown menu for cpus
+        $cpuSelect.select2({
+            data: displayArray,
+            allowClear: true,
+            placeholder: 'Select brand',
+            width: '100%',
+            theme: 'custom'
+        }).trigger('change'); //turns it into a select2 select type so as to make the list searchable
+    });
+}
+
+//displays the CPU based on brand
+$(document).ready( function(){
+    $('.component-select').select2({ //sets all select components to be select 2
+        width: '100%',
+        theme: 'custom'
+    });
+    const $cpuBrandSelect = $("#cpu-select-brand"); //stores jquery id of dropdown menu for cpu BRAND
+    const $cpuSelect = $("#cpu-select");
+    $cpuBrandSelect.on('change', function(){
+        if($cpuBrandSelect.val() == 'intel'){ //gets the value of the select brand dropdown menu and sends the array that goes with the brand select amd cpus and intel cpus
+            $cpuSelect.empty(); 
+            populateDropDown(select2Intelcpus);
+        }
+        if($cpuBrandSelect.val() == 'amd'){
+            $cpuSelect.empty(); 
+            populateDropDown(select2Amdcpus);
+        }
+        if($cpuBrandSelect.val() == ''){
+            $cpuSelect.empty(); 
+        }
+    });
 });
+
+initialize(); //initializes the arrays
+
+
+
