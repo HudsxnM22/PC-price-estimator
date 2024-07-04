112bce37a85a349bfe8a4bb545fe087b535fda1d
HudsxnM22 <maioranahudson@gmail.com>
2024-07-04 15:31:56 -0400

    Changed logo, fixed CPU and GPU select drop downs, styled all dropdowns, adjusted the about section,

--- ComponentLists.js
+++ ComponentLists.js
@@ -4,15 +4,15 @@ intelcpus = [], amdcpus = [], nvidiagpus = [], amdgpus = [], intelgpus = [];
 //grabs cpu array list from back end database
 const FetchComponents = async () => {
     try{    
-        var response = await fetch('https://71.176.233.55:3566/intel-cpus'); //fetches intel cpus and stores them into intel cpu array as strings
+        var response = await fetch('https://pcpricee.xyz:3566/intel-cpus'); //fetches intel cpus and stores them into intel cpu array as strings
         intelcpus = await response.json();
-        var response = await fetch('https://71.176.233.55:3566/amd-cpus'); //fetches amd cpus
+        var response = await fetch('https://pcpricee.xyz:3566/amd-cpus'); //fetches amd cpus
         amdcpus = await response.json();
-        var response = await fetch('https://71.176.233.55:3566/nvidia-gpus'); //[nvidia]fetches gpus and stores them into their respective arrays as strings
+        var response = await fetch('https://pcpricee.xyz:3566/nvidia-gpus'); //[nvidia]fetches gpus and stores them into their respective arrays as strings
         nvidiagpus = await response.json();
-        var response = await fetch('https://71.176.233.55:3566/amd-gpus');//[amd]
+        var response = await fetch('https://pcpricee.xyz:3566/amd-gpus');//[amd]
         amdgpus = await response.json();
-        var response = await fetch('https://71.176.233.55:3566/intel-gpus');//[intel]
+        var response = await fetch('https://pcpricee.xyz:3566/intel-gpus');//[intel]
         intelgpus = await response.json();
     }
     catch(error){
@@ -74,14 +74,14 @@ const initialize = async () => {
     //console.log(amdgpus);
     //console.log(intelgpus);
 
-    populateDropDown();
+    populateCPUDropDown();
+    populateGPUDropDown();
 }
 
 //populate the dropdown menu for cpus via J-query
-const populateDropDown = (displayArray) => {
+const populateCPUDropDown = (displayArray) => {
     $(document).ready( function(){
         const $cpuSelect = $("#cpu-select"); //stores jquery id of dropdown menu for cpus
-        const $gpuSelect = $("#gpu-select");
         $cpuSelect.select2({
             data: displayArray,
             allowClear: true,
@@ -89,22 +89,34 @@ const populateDropDown = (displayArray) => {
             width: '100%',
             theme: 'custom'
         }).trigger('change'); //turns it into a select2 select type so as to make the list searchable
-        $gpuSelect.select2({ //gpus
+    });
+}
+
+const populateGPUDropDown = (displayArray) => {
+    $(document).ready( function(){
+        const $gpuSelect = $("#gpu-select"); //stores jquery id of dropdown menu for gpus
+        $gpuSelect.select2({
             data: displayArray,
             allowClear: true,
             placeholder: 'Select brand',
             width: '100%',
             theme: 'custom'
-        }).trigger('change');
+        }).trigger('change'); //turns it into a select2 select type so as to make the list searchable
     });
 }
 
+
 //displays the CPU based on brand
 $(document).ready( function(){
-    $('.component-select').select2({ //sets all select components to be select 2
-        width: '100%',
-        theme: 'custom'
-    });
+    //$('.component-select').select2({
+    //    width: '100%', //sets all select components to be select 2
+    //    theme: 'custom'
+    //});
+    //$("#motherboard-select-socket").select2({
+    //    allowClear: true,
+    //    width: '100%',
+    //    theme: 'custom'
+    //})
     const $cpuBrandSelect = $("#cpu-select-brand"); //stores jquery id of dropdown menu for cpu BRAND
     const $cpuSelect = $("#cpu-select");
     const $gpuBrandSelect = $("#gpu-select-brand"); //stores jquery id of dropdown menu for gpu BRAND
@@ -112,31 +124,31 @@ $(document).ready( function(){
     $cpuBrandSelect.on('change', function(){
         if($cpuBrandSelect.val() == 'intel'){ //gets the value of the select brand dropdown menu and sends the array that goes with the brand select amd cpus and intel cpus
             $cpuSelect.empty(); 
-            populateDropDown(select2Intelcpus);
+            populateCPUDropDown(select2Intelcpus);
         }
         if($cpuBrandSelect.val() == 'amd'){
             $cpuSelect.empty(); 
-            populateDropDown(select2Amdcpus);
+            populateCPUDropDown(select2Amdcpus);
         }
         if($cpuBrandSelect.val() == ''){
             $cpuSelect.empty(); 
         }
     });
-    $gpuBrandSelect.on('change', function(){
+    $gpuBrandSelect.on('change', function(){ //when  the user changes the brand it
         if($gpuBrandSelect.val() == 'nvidia'){ //gets the value of the select brand dropdown menu and sends the array that goes with the brand select amd GPUS and intel GPUS and nnvidia GPUS
             $gpuSelect.empty(); 
-            populateDropDown(select2Nvidiagpus);
+            populateGPUDropDown(select2Nvidiagpus);
         }
         if($gpuBrandSelect.val() == 'amd'){
             $gpuSelect.empty(); 
-            populateDropDown(select2Amdgpus);
+            populateGPUDropDown(select2Amdgpus);
         }
         if($gpuBrandSelect.val() == 'intel'){
             $gpuSelect.empty(); 
-            populateDropDown(select2Intelgpus);
+            populateGPUDropDown(select2Intelgpus);
         }
         if($gpuBrandSelect.val() == ''){
-            $cpuSelect.empty(); 
+            $gpuSelect.empty(); 
         }
     });
 });
